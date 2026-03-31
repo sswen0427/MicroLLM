ef38c66a06a3e86c884ad8c3c67965e3adbf23e8
sswen0427 <799438891@qq.com>
2026-03-31 21:57:05 +0800

    add src code

--- src/buffer/buffer.cpp
+++ src/buffer/buffer.cpp
@@ -1,3 +1,47 @@
-//
-// Created by wensisi.0427 on 3/29/26.
-//
+
+#include "buffer.h"
+Buffer::Buffer(std::size_t byte_size,
+               std::shared_ptr<DeviceAllocator> allocator, void *ptr,
+               bool use_external)
+    : byte_size_(byte_size),
+      allocator_(allocator),
+      ptr_(ptr),
+      use_external_(use_external) {
+  if (!ptr_ && allocator_) {
+    use_external_ = false;
+    ptr_ = allocator_->allocate(byte_size_);
+  }
+}
+
+Buffer::~Buffer() {
+  if (!use_external_) {
+    if (ptr_ && allocator_) {
+      allocator_->release(ptr_);
+      ptr_ = nullptr;
+    }
+  }
+}
+
+void *Buffer::ptr() { return ptr_; }
+
+const void *Buffer::ptr() const { return ptr_; }
+
+size_t Buffer::byte_size() const { return byte_size_; }
+
+bool Buffer::allocate() {
+  if (allocator_ && byte_size_ != 0) {
+    use_external_ = false;
+    ptr_ = allocator_->allocate(byte_size_);
+    if (!ptr_) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+
+std::shared_ptr<DeviceAllocator> Buffer::allocator() const {
+  return allocator_;
+}
