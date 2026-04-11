aaa53005c16eb7d9ffdaeb4bb226297fefdef233
sswen0427 <799438891@qq.com>
2026-04-11 18:36:05 +0800

    format test code

--- format.sh
+++ format.sh
@@ -2,4 +2,5 @@
 
 find include/ -iname '*.h' -print0 -o -iname '*.cpp' -print0 | xargs -0 clang-format -i --style=Google
 find src/ -iname '*.h' -print0 -o -iname '*.cpp' -print0 | xargs -0 clang-format -i --style=Google
+find test/ -iname '*.h' -print0 -o -iname '*.cpp' -print0 | xargs -0 clang-format -i --style=Google
 clang-format -i --style=Google main.cpp
