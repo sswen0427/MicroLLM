ab075e2a09f697a0d45ebd6175e8bf1c9ce54838
sswen0427 <799438891@qq.com>
2026-04-12 01:47:48 +0800

    add tensor.h

--- include/tensor/tensor.h
+++ include/tensor/tensor.h
@@ -7,23 +7,38 @@
 
 #include "base/buffer.h"
 
+
 namespace tensor {
 
 class Tensor {
  public:
   explicit Tensor() = default;
 
-  explicit Tensor(base::DataType data_type, int32_t dim0);
+  explicit Tensor(base::DataType data_type, int32_t dim0, bool need_alloc = false,
+                  std::shared_ptr<base::DeviceAllocator> alloc = nullptr, void* ptr = nullptr);
 
-  explicit Tensor(base::DataType data_type, int32_t dim0, int32_t dim1);
+  explicit Tensor(base::DataType data_type, int32_t dim0, int32_t dim1, bool need_alloc = false,
+                  std::shared_ptr<base::DeviceAllocator> alloc = nullptr, void* ptr = nullptr);
 
-  explicit Tensor(base::DataType data_type, int32_t dim0, int32_t dim1,
-                  int32_t dim2);
+  explicit Tensor(base::DataType data_type, int32_t dim0, int32_t dim1, int32_t dim2,
+                  bool need_alloc = false, std::shared_ptr<base::DeviceAllocator> alloc = nullptr,
+                  void* ptr = nullptr);
 
-  explicit Tensor(base::DataType data_type, int32_t dim0, int32_t dim1,
-                  int32_t dim2, int32_t dim3);
+  explicit Tensor(base::DataType data_type, int32_t dim0, int32_t dim1, int32_t dim2, int32_t dim3,
+                  bool need_alloc = false, std::shared_ptr<base::DeviceAllocator> alloc = nullptr,
+                  void* ptr = nullptr);
 
-  explicit Tensor(base::DataType data_type, std::vector<int32_t> dims);
+  explicit Tensor(base::DataType data_type, std::vector<int32_t> dims, bool need_alloc = false,
+                  std::shared_ptr<base::DeviceAllocator> alloc = nullptr, void* ptr = nullptr);
+
+  void to_cpu();
+
+  void to_cuda(cudaStream_t stream = nullptr);
+
+  bool is_empty() const;
+
+  void init_buffer(std::shared_ptr<base::DeviceAllocator> alloc, base::DataType data_type,
+                   bool need_alloc, void* ptr);
 
   template <typename T>
   T* ptr();
@@ -31,11 +46,9 @@ class Tensor {
   template <typename T>
   const T* ptr() const;
 
-  template <typename T>
-  T& index(int64_t offset);
+  void reshape(const std::vector<int32_t>& dims);
 
-  template <typename T>
-  const T& index(int64_t offset) const;
+  std::shared_ptr<base::Buffer> get_buffer() const;
 
   size_t size() const;
 
@@ -47,8 +60,6 @@ class Tensor {
 
   int32_t get_dim(int32_t idx) const;
 
-  void reshape(std::vector<int32_t> dims);
-
   const std::vector<int32_t>& dims() const;
 
   std::vector<size_t> strides() const;
@@ -57,25 +68,39 @@ class Tensor {
 
   void reset(base::DataType data_type, const std::vector<int32_t>& dims);
 
-  bool allocate(std::shared_ptr<base::DeviceAllocator> allocator,
-                bool need_realloc = false);
+  void set_device_type(base::DeviceType device_type) const;
+
+  base::DeviceType device_type() const;
+
+  bool allocate(std::shared_ptr<base::DeviceAllocator> allocator, bool need_realloc = false);
+
+  template <typename T>
+  T* ptr(int64_t index);
+
+  template <typename T>
+  const T* ptr(int64_t index) const;
+
+  template <typename T>
+  T& index(int64_t offset);
+
+  template <typename T>
+  const T& index(int64_t offset) const;
+
+  tensor::Tensor clone() const;
 
  private:
   size_t size_ = 0;
-
   std::vector<int32_t> dims_;
-
   std::shared_ptr<base::Buffer> buffer_;
-
   base::DataType data_type_ = base::DataType::kDataTypeUnknown;
 };
 
 template <typename T>
 T& Tensor::index(int64_t offset) {
-  // CHECK_GE(offset, 0);
-  // CHECK_LT(offset, this->size());
-  // T& val = *(reinterpret_cast<T*>(buffer_->ptr()) + offset);
-  // return val;
+  CHECK_GE(offset, 0);
+  CHECK_LT(offset, this->size());
+  T& val = *(reinterpret_cast<T*>(buffer_->ptr()) + offset);
+  return val;
 }
 
 template <typename T>
@@ -91,7 +116,7 @@ const T* Tensor::ptr() const {
   if (!buffer_) {
     return nullptr;
   }
-  return const_cast<const T*>(static_cast<T*>(buffer_->ptr()));
+  return const_cast<const T*>(reinterpret_cast<T*>(buffer_->ptr()));
 }
 
 template <typename T>
@@ -99,8 +124,24 @@ T* Tensor::ptr() {
   if (!buffer_) {
     return nullptr;
   }
-  return static_cast<T*>(buffer_->ptr());
+  return reinterpret_cast<T*>(buffer_->ptr());
+}
+
+template <typename T>
+T* Tensor::ptr(int64_t index) {
+  CHECK(buffer_ != nullptr && buffer_->ptr() != nullptr)
+      << "The data area buffer of this tensor is empty or it points to a null pointer.";
+  return const_cast<T*>(reinterpret_cast<const T*>(buffer_->ptr())) + index;
+}
+
+template <typename T>
+const T* Tensor::ptr(int64_t index) const {
+  CHECK(buffer_ != nullptr && buffer_->ptr() != nullptr)
+      << "The data area buffer of this tensor is empty or it points to a null pointer.";
+  return reinterpret_cast<const T*>(buffer_->ptr()) + index;
 }
 }  // namespace tensor
+
+
 
 #endif  // MICROLLM_INCLUDE_TENSOR_TENSOR_H
