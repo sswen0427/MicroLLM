63c03e385fcb456eafcf9c09a0fdec130f766f05
sswen0427 <799438891@qq.com>
2026-04-14 23:22:04 +0800

    fix ut

--- test/test_tensor/test_tensor.cpp
+++ test/test_tensor/test_tensor.cpp
@@ -50,56 +50,49 @@ TEST(TensorTest, TOCUDA) {
   }
 }
 
-// TEST(test_tensor, clone_cuda) {
-//   using namespace base;
-//   auto alloc_cu = CUDADeviceAllocatorFactory::get_instance();
-//   tensor::Tensor t1_cu(DataType::kDataTypeFp32, 32, 32, true, alloc_cu);
-//   ASSERT_EQ(t1_cu.is_empty(), false);
-//   set_value_cu(t1_cu.ptr<float>(), 32 * 32, 1.f);
-//
-//   tensor::Tensor t2_cu = t1_cu.clone();
-//   float* p2 = new float[32 * 32];
-//   cudaMemcpy(p2, t2_cu.ptr<float>(), sizeof(float) * 32 * 32,
-//   cudaMemcpyDeviceToHost); for (int i = 0; i < 32 * 32; ++i) {
-//     ASSERT_EQ(p2[i], 1.f);
-//   }
-//
-//   cudaMemcpy(p2, t1_cu.ptr<float>(), sizeof(float) * 32 * 32,
-//   cudaMemcpyDeviceToHost); for (int i = 0; i < 32 * 32; ++i) {
-//     ASSERT_EQ(p2[i], 1.f);
-//   }
-//
-//   ASSERT_EQ(t2_cu.data_type(), base::DataType::kDataTypeFp32);
-//   ASSERT_EQ(t2_cu.size(), 32 * 32);
-//
-//   t2_cu.to_cpu();
-//   std::memcpy(p2, t2_cu.ptr<float>(), sizeof(float) * 32 * 32);
-//   for (int i = 0; i < 32 * 32; ++i) {
-//     ASSERT_EQ(p2[i], 1.f);
-//   }
-//   delete[] p2;
-// }
-//
-// TEST(test_tensor, clone_cpu) {
-//   using namespace base;
-//   auto alloc_cpu = CPUDeviceAllocatorFactory::get_instance();
-//   tensor::Tensor t1_cpu(DataType::kDataTypeFp32, 32, 32, true, alloc_cpu);
-//   ASSERT_EQ(t1_cpu.is_empty(), false);
-//   for (int i = 0; i < 32 * 32; ++i) {
-//     t1_cpu.index<float>(i) = 1.f;
-//   }
-//
-//   tensor::Tensor t2_cpu = t1_cpu.clone();
-//   float* p2 = new float[32 * 32];
-//   std::memcpy(p2, t2_cpu.ptr<float>(), sizeof(float) * 32 * 32);
-//   for (int i = 0; i < 32 * 32; ++i) {
-//     ASSERT_EQ(p2[i], 1.f);
-//   }
-//
-//   std::memcpy(p2, t1_cpu.ptr<float>(), sizeof(float) * 32 * 32);
-//   for (int i = 0; i < 32 * 32; ++i) {
-//     ASSERT_EQ(p2[i], 1.f);
-//   }
-//   delete[] p2;
-// }
-//
+TEST(TensorTest, CloneCUDA) {
+  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
+  tensor::Tensor t1_cu = tensor::Tensor::allocate(
+      base::DataType::kDataTypeInt32, {32, 32}, alloc_cu);
+  CHECK(!t1_cu.is_empty());
+  int array[32 * 32];
+  for (int i = 0; i < 32 * 32; i++) {
+    array[i] = i;
+  }
+  cudaMemcpy(t1_cu.ptr<int>(), array, 32 * 32 * 4, cudaMemcpyHostToDevice);
+
+  int expected_array[32 * 32];
+  tensor::Tensor t2_cu = t1_cu.clone();
+  cudaMemcpy(expected_array, t2_cu.ptr<float>(), sizeof(float) * 32 * 32,
+             cudaMemcpyDeviceToHost);
+  for (int i = 0; i < 32 * 32; ++i) {
+    EXPECT_EQ(expected_array[i], i);
+  }
+  EXPECT_EQ(t2_cu.data_type(), base::DataType::kDataTypeInt32);
+  EXPECT_EQ(t2_cu.size(), 32 * 32);
+
+  t2_cu.to_cpu();
+  int* cpu_ptr = t2_cu.ptr<int>();
+  for (int i = 0; i < 32 * 32; ++i) {
+    CHECK_EQ(*(cpu_ptr + i), i);
+  }
+}
+
+TEST(TensorTest, CloneCPU) {
+  auto alloc_cpu = base::CPUDeviceAllocatorFactory::get_instance();
+  tensor::Tensor t1_cpu = tensor::Tensor::allocate(
+      base::DataType::kDataTypeInt32, {32, 32}, alloc_cpu);
+  CHECK(!t1_cpu.is_empty());
+  for (int i = 0; i < 32 * 32; ++i) {
+    t1_cpu.at<int>(i) = i;
+  }
+
+  tensor::Tensor t2_cpu = t1_cpu.clone();
+  int expected_array[32 * 32];
+  std::memcpy(expected_array, t2_cpu.ptr<int>(), 32 * 32 * 4);
+  for (int i = 0; i < 32 * 32; ++i) {
+    EXPECT_EQ(expected_array[i], i);
+  }
+  EXPECT_EQ(t2_cpu.data_type(), base::DataType::kDataTypeInt32);
+  EXPECT_EQ(t2_cpu.size(), 32 * 32);
+}
