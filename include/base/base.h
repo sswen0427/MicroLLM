404e2957a3336cbc8d788ee6606f8b32d84bdcef
sswen0427 <799438891@qq.com>
2026-04-15 01:50:08 +0800

    fix

--- include/base/base.h
+++ include/base/base.h
@@ -5,29 +5,6 @@
 #include <glog/logging.h>
 
 #include <string>
-namespace model {
-enum class ModelBufferType {
-  kInputTokens = 0,
-  kInputEmbeddings = 1,
-  kOutputRMSNorm = 2,
-  kKeyCache = 3,
-  kValueCache = 4,
-  kQuery = 5,
-  kInputPos = 6,
-  kScoreStorage = 7,
-  kOutputMHA = 8,
-  kAttnOutput = 9,
-  kW1Output = 10,
-  kW2Output = 11,
-  kW3Output = 12,
-  kFFNRMSNorm = 13,
-  kForwardOutput = 15,
-  kForwardOutputCPU = 16,
-
-  kSinCache = 17,
-  kCosCache = 18,
-};
-}
 
 namespace base {
 enum class DeviceType : uint8_t {
