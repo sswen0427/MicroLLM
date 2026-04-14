d1c84f5be9adfd2a2b1205e1909c045dc4119e2c
sswen <60056687+sswen0427@users.noreply.github.com>
2026-04-15 02:40:05 +0800

    Op layer (#22)
    * fix
    
    * add layer.h
    
    * polish code
    
    * add layer.cpp
    
    * remove class Layer;
    
    * add BaseLayer implementation
    
    * remove init
    
    * remove init
    
    * fix forward
    
    * add CudaConfig
    
    * Layer
    
    * compile layer

--- include/op/layer.h
+++ include/op/layer.h
@@ -6,87 +6,163 @@
 #include "tensor/tensor.h"
 
 namespace op {
+
 enum class LayerType : uint8_t {
   kLayerUnknown = 0,
   kLayerLinear = 1,
+  kLayerEncode = 2,
+  kLayerEmbedding = 3,
+  kLayerRMSNorm = 4,
+  kLayerMatmul = 5,
+  kLayerRoPe = 6,
+  kLayerMHA = 7,
+  kLayerSoftmax = 8,
+  kLayerAdd = 9,
+  kLayerSwiGLU = 10,
 };
 
-enum class LayerStatus : uint8_t {
-  kForwardSuccess = 0,
-  kFunctionUnImplement = 1,
-};
-
-class Layer {
+class BaseLayer {
  public:
-  explicit Layer(LayerType layer_type, base::DataType data_type,
-                 std::string layer_name = "");
+  explicit BaseLayer(base::DeviceType device_type, LayerType layer_type,
+                     base::DataType data_type, std::string layer_name = "");
 
-  base::DataType data_type() const;
+  virtual ~BaseLayer() = default;
 
-  LayerType layer_type() const;
+  virtual base::Status forward() = 0;
 
-  virtual LayerStatus Init() = 0;
-
-  virtual LayerStatus Forward() = 0;
+  virtual base::Status forward(const std::vector<tensor::Tensor>& inputs,
+                               std::vector<tensor::Tensor>& outputs) = 0;
 
   virtual void set_input(int32_t idx, const tensor::Tensor& input) = 0;
 
   virtual void set_output(int32_t idx, const tensor::Tensor& output) = 0;
 
-  virtual tensor::Tensor get_input(int32_t idx) const = 0;
+  virtual size_t input_size() const = 0;
 
-  virtual tensor::Tensor get_output(int32_t idx) const = 0;
+  virtual size_t output_size() const = 0;
 
-  virtual void set_weight(int32_t idx, const tensor::Tensor& weight) = 0;
+  virtual base::Status check() const = 0;
 
-  virtual tensor::Tensor get_weight(int32_t idx) const = 0;
+  virtual tensor::Tensor& get_input(int32_t idx) = 0;
 
-  virtual void reset_input_size(size_t size) = 0;
+  virtual tensor::Tensor& get_output(int32_t idx) = 0;
 
-  virtual void reset_output_size(size_t size) = 0;
+  virtual const tensor::Tensor& get_input(int32_t idx) const = 0;
 
- private:
+  virtual const tensor::Tensor& get_output(int32_t idx) const = 0;
+
+  virtual base::Status set_weight(int32_t idx,
+                                  const tensor::Tensor& weight) = 0;
+
+  virtual base::Status set_weight(
+      int32_t idx, const std::vector<int32_t>& dims, const void* weight_ptr,
+      base::DeviceType device_type = base::DeviceType::kDeviceUnknown) = 0;
+
+  base::DataType data_type() const;
+
+  LayerType layer_type() const;
+
+  const std::string& get_layer_name() const;
+
+  void set_layer_name(const std::string& layer_name);
+
+  base::DeviceType device_type() const;
+
+  void set_device_type(base::DeviceType device_type);
+
+ protected:
   std::string layer_name_;
-
-  base::DataType data_type_ = base::DataType::kDataTypeUnknown;
-
   LayerType layer_type_ = LayerType::kLayerUnknown;
+  base::DataType data_type_ = base::DataType::kDataTypeUnknown;
+  base::DeviceType device_type_ = base::DeviceType::kDeviceUnknown;
 };
 
-class ParamLayerFp32 : public Layer {
+class Layer : public BaseLayer {
  public:
-  explicit ParamLayerFp32(LayerType layer_type, std::string layer_name = "");
+  explicit Layer(base::DeviceType device_type, LayerType layer_type,
+                 std::string layer_name = "");
 
-  LayerStatus Init() override;
+  base::Status forward() override;
 
-  LayerStatus Forward() override;
+  base::Status forward(const std::vector<tensor::Tensor>& inputs,
+                       std::vector<tensor::Tensor>& outputs) override;
 
   void set_input(int32_t idx, const tensor::Tensor& input) override;
 
   void set_output(int32_t idx, const tensor::Tensor& output) override;
 
-  tensor::Tensor get_input(int32_t idx) const override;
+  size_t input_size() const override;
 
-  tensor::Tensor get_output(int32_t idx) const override;
+  size_t output_size() const override;
 
-  void set_weight(int32_t idx, const tensor::Tensor& weight) override;
+  base::Status check() const override;
 
-  tensor::Tensor get_weight(int32_t idx) const override;
+  tensor::Tensor& get_input(int32_t idx) override;
+
+  tensor::Tensor& get_output(int32_t idx) override;
+
+  const tensor::Tensor& get_input(int32_t idx) const override;
+
+  const tensor::Tensor& get_output(int32_t idx) const override;
+
+  base::Status check_tensor(const tensor::Tensor& tensor,
+                            base::DeviceType device_type,
+                            base::DataType data_type) const;
+
+  base::Status check_tensor_with_dim(const tensor::Tensor& tensor,
+                                     base::DeviceType device_type,
+                                     base::DataType data_type,
+                                     std::initializer_list<int32_t> dims) const;
+
+  void reset_input_size(size_t size);
+
+  void reset_output_size(size_t size);
+
+  virtual void to_cuda();
+
+  void set_cuda_config(std::shared_ptr<base::CudaConfig> config);
+
+  std::shared_ptr<base::CudaConfig> cuda_config() const;
+
+ protected:
+  std::vector<tensor::Tensor> inputs_;
+  std::vector<tensor::Tensor> outputs_;
+  std::shared_ptr<base::CudaConfig> cuda_config_;
+};
+
+class LayerParam : public Layer {
+ public:
+  explicit LayerParam(base::DeviceType device_type, LayerType layer_type,
+                      bool is_quant_layer = false, std::string layer_name = "");
+
+  void to_cuda() override;
+
+  base::Status set_weight(int32_t idx, const tensor::Tensor& weight) override;
+
+  base::Status set_weight(
+      int32_t idx, const std::vector<int32_t>& dims, const void* weight_ptr,
+      base::DeviceType device_type = base::DeviceType::kDeviceUnknown) override;
+
+  size_t weight_size() const;
+
+  const tensor::Tensor& get_weight(int32_t idx) const;
+
+  int32_t get_scale_num() const;
 
   void reset_weight_size(size_t size);
 
-  void reset_input_size(size_t size) override;
+  tensor::Tensor& get_weight(int32_t idx);
 
-  void reset_output_size(size_t size) override;
+  void set_scales(const tensor::Tensor& scales);
 
- private:
+  void set_group_size(int32_t group_size);
+
+ protected:
+  int32_t group_size_ = 0;
+  bool is_quant_layer_ = false;
+  tensor::Tensor scales_;
   std::vector<tensor::Tensor> weights_;
-
-  std::vector<tensor::Tensor> inputs_;
-
-  std::vector<tensor::Tensor> outputs_;
 };
-
 }  // namespace op
 
 #endif  // MICROLLM_INCLUDE_OP_LAYER_H
--- src/op/layer.cpp
+++ src/op/layer.cpp
@@ -1,72 +1,270 @@
-
 #include "layer.h"
+
+#include <numeric>
 
 #include "base/base.h"
 #include "glog/logging.h"
 #include "tensor/tensor.h"
 
+// BaseLayer
 namespace op {
-Layer::Layer(LayerType layer_type, base::DataType data_type,
-             std::string layer_name)
-    : layer_type_(layer_type),
+BaseLayer::BaseLayer(base::DeviceType device_type, LayerType layer_type,
+                     base::DataType data_type, std::string layer_name)
+    : device_type_(device_type),
+      layer_type_(layer_type),
       data_type_(data_type),
       layer_name_(std::move(layer_name)) {}
 
-base::DataType Layer::data_type() const { return data_type_; }
+base::DataType BaseLayer::data_type() const { return data_type_; }
 
-LayerType Layer::layer_type() const { return layer_type_; }
+LayerType BaseLayer::layer_type() const { return layer_type_; }
 
-ParamLayerFp32::ParamLayerFp32(LayerType layer_type, std::string layer_name)
-    : Layer(layer_type, base::DataType::kDataTypeFp32, std::move(layer_name)) {}
+const std::string& BaseLayer::get_layer_name() const { return layer_name_; }
 
-LayerStatus ParamLayerFp32::Init() { return LayerStatus::kFunctionUnImplement; }
-
-LayerStatus ParamLayerFp32::Forward() {
-  return LayerStatus::kFunctionUnImplement;
+void BaseLayer::set_layer_name(const std::string& layer_name) {
+  layer_name_ = layer_name;
 }
+base::DeviceType BaseLayer::device_type() const { return device_type_; }
 
-void ParamLayerFp32::set_input(int32_t idx, const tensor::Tensor &input) {
+void BaseLayer::set_device_type(base::DeviceType device_type) {
+  device_type_ = device_type;
+}
+}  // namespace op
+
+// Layer
+namespace op {
+Layer::Layer(base::DeviceType device_type, LayerType layer_type,
+             std::string layer_name)
+    : BaseLayer(device_type, layer_type, base::DataType::kDataTypeFp32,
+                std::move(layer_name)) {}
+
+base::Status Layer::forward() { return base::error::FunctionNotImplement(""); }
+
+base::Status Layer::forward(const std::vector<tensor::Tensor>& inputs,
+                            std::vector<tensor::Tensor>& outputs) {
+  for (const auto& input : inputs) {
+    inputs_.emplace_back(input);
+  }
+  for (const auto& output : outputs) {
+    outputs_.emplace_back(output);
+  }
+  return this->forward();
+}
+void Layer::set_input(int32_t idx, const tensor::Tensor& input) {
   CHECK_GE(idx, 0);
-  CHECK_LT(idx, input.size());
-  CHECK(input.data_type() == base::DataType::kDataTypeFp32);
-  inputs_.at(idx) = input;
+  CHECK_LT(idx, inputs_.size());
+  this->inputs_.at(idx) = input;
 }
 
-void ParamLayerFp32::set_output(int32_t idx, const tensor::Tensor &output) {
+void Layer::set_output(int32_t idx, const tensor::Tensor& output) {
   CHECK_GE(idx, 0);
-  CHECK_LT(idx, output.size());
-  CHECK(output.data_type() == base::DataType::kDataTypeFp32);
-  outputs_.at(idx) = output;
+  CHECK_LT(idx, outputs_.size());
+  this->outputs_.at(idx) = output;
 }
 
-tensor::Tensor ParamLayerFp32::get_input(int32_t idx) const {
+size_t Layer::input_size() const { return inputs_.size(); }
+
+size_t Layer::output_size() const { return outputs_.size(); }
+base::Status Layer::check() const {
+  return base::error::FunctionNotImplement(
+      "The check function is not implement yet");
+}
+tensor::Tensor& Layer::get_input(int32_t idx) {
   CHECK_GE(idx, 0);
   CHECK_LT(idx, inputs_.size());
   return inputs_.at(idx);
 }
 
-tensor::Tensor ParamLayerFp32::get_output(int32_t idx) const {
+tensor::Tensor& Layer::get_output(int32_t idx) {
   CHECK_GE(idx, 0);
   CHECK_LT(idx, outputs_.size());
   return outputs_.at(idx);
 }
 
-void ParamLayerFp32::set_weight(int32_t idx, const tensor::Tensor &weight) {
+const tensor::Tensor& Layer::get_input(int32_t idx) const {
   CHECK_GE(idx, 0);
-  CHECK_LT(idx, weight.size());
-  CHECK(weight.data_type() == base::DataType::kDataTypeFp32);
-  weights_.at(idx) = weight;
+  CHECK_LT(idx, inputs_.size());
+  return inputs_.at(idx);
 }
 
-tensor::Tensor ParamLayerFp32::get_weight(int32_t idx) const {
+const tensor::Tensor& Layer::get_output(int32_t idx) const {
+  CHECK_GE(idx, 0);
+  CHECK_LT(idx, outputs_.size());
+  return outputs_.at(idx);
+}
+
+base::Status Layer::check_tensor(const tensor::Tensor& tensor,
+                                 base::DeviceType device_type,
+                                 base::DataType data_type) const {
+  if (tensor.is_empty()) {
+    return base::error::InvalidArgument("The tensor parameter is empty.");
+  }
+  if (tensor.device_type() != device_type) {
+    return base::error::InvalidArgument("The tensor has a wrong device type.");
+  }
+  if (tensor.data_type() != data_type) {
+    return base::error::InvalidArgument("The tensor has a wrong data type.");
+  }
+  return base::error::Success();
+}
+
+base::Status Layer::check_tensor_with_dim(
+    const tensor::Tensor& tensor, base::DeviceType device_type,
+    base::DataType data_type, std::initializer_list<int32_t> dims) const {
+  if (tensor.is_empty()) {
+    return base::error::InvalidArgument("The tensor parameter is empty.");
+  }
+  if (tensor.device_type() != device_type) {
+    return base::error::InvalidArgument("The tensor has a wrong device type.");
+  }
+  if (tensor.data_type() != data_type) {
+    return base::error::InvalidArgument("The tensor has a wrong data type.");
+  }
+  if (tensor.dims_size() != static_cast<int32_t>(dims.size())) {
+    return base::error::InvalidArgument(
+        "The tensor dimension count mismatch. Expected: " +
+        std::to_string(dims.size()) +
+        ", Got: " + std::to_string(tensor.dims_size()));
+  }
+  int32_t i = 0;
+  for (int32_t expected_dim : dims) {
+    if (tensor.get_dim(i) != expected_dim) {
+      return base::error::InvalidArgument(
+          "The tensor has a wrong dim at index " + std::to_string(i) +
+          ". Expected: " + std::to_string(expected_dim) +
+          ", Got: " + std::to_string(tensor.get_dim(i)));
+    }
+    ++i;
+  }
+  return base::error::Success();
+}
+
+void Layer::reset_input_size(size_t size) { inputs_.resize(size); }
+
+void Layer::reset_output_size(size_t size) { outputs_.resize(size); }
+
+void Layer::to_cuda() {
+  for (auto& input : inputs_) {
+    if (!input.is_empty()) {
+      input.to_cuda(cuda_config_ ? cuda_config_->stream : nullptr);
+    }
+  }
+  for (auto& output : outputs_) {
+    if (!output.is_empty()) {
+      output.to_cuda(cuda_config_ ? cuda_config_->stream : nullptr);
+    }
+  }
+}
+
+void Layer::set_cuda_config(std::shared_ptr<base::CudaConfig> config) {
+  if (!config) {
+    return;
+  }
+  this->cuda_config_ = config;
+}
+
+std::shared_ptr<base::CudaConfig> Layer::cuda_config() const {
+  return cuda_config_;
+}
+}  // namespace op
+
+// LayerParam
+namespace op {
+LayerParam::LayerParam(base::DeviceType device_type, LayerType layer_type,
+                       bool is_quant_layer, std::string layer_name)
+    : Layer(device_type, layer_type, std::move(layer_name)),
+      is_quant_layer_(is_quant_layer) {}
+
+void LayerParam::to_cuda() {
+  Layer::to_cuda();
+  for (auto& weight : weights_) {
+    weight.to_cuda(cuda_config_ ? cuda_config_->stream : nullptr);
+  }
+  if (!scales_.is_empty()) {
+    scales_.to_cuda(cuda_config_ ? cuda_config_->stream : nullptr);
+  }
+}
+
+base::Status LayerParam::set_weight(int32_t idx, const tensor::Tensor& weight) {
+  CHECK_GE(idx, 0);
+  CHECK_LT(idx, weights_.size());
+  CHECK(weight.data_type() == base::DataType::kDataTypeFp32);
+  if (!weight.is_empty()) {
+    CHECK(weight.device_type() == device_type_);
+  }
+  weights_.at(idx) = weight;
+  return base::error::Success();
+}
+
+base::Status LayerParam::set_weight(int32_t idx,
+                                    const std::vector<int32_t>& dims,
+                                    const void* weight_ptr,
+                                    base::DeviceType device_type) {
+  CHECK_GE(idx, 0);
+  CHECK_LT(idx, weights_.size());
+  CHECK_NE(weight_ptr, nullptr);
+
+  size_t size = std::accumulate(dims.begin(), dims.end(), sizeof(float),
+                                std::multiplies<>());
+  std::shared_ptr<base::Buffer> buffer = std::make_shared<base::Buffer>(
+      size, nullptr, const_cast<void*>(weight_ptr));
+  if (device_type != base::DeviceType::kDeviceUnknown) {
+    buffer->set_device_type(device_type);
+  }
+
+  if (!is_quant_layer_) {
+    tensor::Tensor weight = tensor::Tensor::from_external(
+        base::DataType::kDataTypeFp32, dims, buffer.get());
+    weight.set_device_type(device_type);
+    weights_.at(idx) = weight;
+  } else {
+    // is quant layer
+    tensor::Tensor weight = tensor::Tensor::from_external(
+        base::DataType::kDataTypeInt8, dims, buffer.get());
+    weight.set_device_type(device_type);
+    weights_.at(idx) = weight;
+
+    const int32_t weight_size = static_cast<int32_t>(weight.size());
+    CHECK(weight_size % group_size_ == 0);
+
+    int32_t scale_nums = weight_size / group_size_;
+    scales_ = tensor::Tensor::from_external(base::DataType::kDataTypeFp32,
+                                            {scale_nums},
+                                            (int8_t*)weight_ptr + weight_size);
+    scales_.set_device_type(device_type);
+  }
+
+  return base::error::Success();
+}
+
+size_t LayerParam::weight_size() const { return weights_.size(); }
+
+const tensor::Tensor& LayerParam::get_weight(int32_t idx) const {
   CHECK_GE(idx, 0);
   CHECK_LT(idx, weights_.size());
   return weights_.at(idx);
 }
 
-void ParamLayerFp32::reset_input_size(size_t size) { inputs_.resize(size); }
+int32_t LayerParam::get_scale_num() const {
+  CHECK(!scales_.is_empty());
+  return static_cast<int32_t>(scales_.size());
+}
 
-void ParamLayerFp32::reset_output_size(size_t size) { outputs_.resize(size); }
+void LayerParam::reset_weight_size(size_t size) { weights_.resize(size); }
 
-void ParamLayerFp32::reset_weight_size(size_t size) { weights_.resize(size); }
+tensor::Tensor& LayerParam::get_weight(int32_t idx) {
+  CHECK_GE(idx, 0);
+  CHECK_LT(idx, weights_.size());
+  return weights_.at(idx);
+}
+
+void LayerParam::set_scales(const tensor::Tensor& scales) {
+  CHECK(!scales.is_empty());
+  this->scales_ = scales;
+}
+
+void LayerParam::set_group_size(int32_t group_size) {
+  this->group_size_ = group_size;
+}
+
 }  // namespace op
