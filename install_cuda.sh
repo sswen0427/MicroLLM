11cd1928aa0a5b350534d71d355c1d782dffa495
sswen0427 <799438891@qq.com>
2026-04-19 15:08:56 +0800

    cuda 12.8

--- install_cuda.sh
+++ install_cuda.sh
@@ -2,5 +2,5 @@
 wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda_12.8.0_570.86.10_linux.run
 sudo sh cuda_12.8.0_570.86.10_linux.run
 
-export PATH=/usr/local/cuda-12.1/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-12.1/lib64:$LD_LIBRARY_PATH
+export PATH=/usr/local/cuda-12.8/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-12.8/lib64:$LD_LIBRARY_PATH
