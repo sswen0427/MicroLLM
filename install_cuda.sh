200301b265f396982120011750e7eacade256ab3
sswen0427 <799438891@qq.com>
2026-04-19 14:11:08 +0800

    cuda 12.8.0

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,7 +1,6 @@
 # Install cuda without driver
-wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run
-chmod +x cuda_12.1.0_530.30.02_linux.run
-./cuda_12.1.0_530.30.02_linux.run
+wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda_12.8.0_570.86.10_linux.run
+sudo sh cuda_12.8.0_570.86.10_linux.run
 
 export PATH=/usr/local/cuda-12.1/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-12.1/lib64:$LD_LIBRARY_PATH
