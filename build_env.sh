26016ea333fe35ce0eb1d7940b3e46da0f0b04e8
sswen0427 <799438891@qq.com>
2026-04-19 16:16:37 +0800

    add build_env.sh

--- build_env.sh
+++ build_env.sh
@@ -4,3 +4,10 @@ sudo sh cuda_12.8.0_570.86.10_linux.run
 
 export PATH=/usr/local/cuda-12.8/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-12.8/lib64:$LD_LIBRARY_PATH
+
+# Install cmake
+cd /tmp
+wget https://github.com/Kitware/CMake/releases/download/v4.3.1/cmake-4.3.1-linux-x86_64.tar.gz
+tar -zxvf cmake-4.3.1-linux-x86_64.tar.gz
+sudo mv cmake-4.3.1-linux-x86_64 /usr/local/cmake
+export PATH=/usr/local/cmake/bin:$PATH
