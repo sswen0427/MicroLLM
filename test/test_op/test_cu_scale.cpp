efbc0208dd55be08fc5ce337dae2a5d20e4056af
sswen0427 <799438891@qq.com>
2026-04-20 13:10:24 +0800

    remove comment

--- test/test_op/test_cu_scale.cpp
+++ test/test_op/test_cu_scale.cpp
@@ -4,36 +4,43 @@
 
 #include "base/buffer.h"
 #include "op/kernels/kernels_interface.h"
-// TEST(test_scale_cu, scale1_nostream) {
-//   auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
-//   int32_t size = 32 * 151;
-//
-//   tensor::Tensor t1(base::DataType::kDataTypeFp32, size, true, alloc_cu);
-//   set_value_cu(static_cast<float*>(t1.get_buffer()->ptr()), size, 2.f);
-//   kernel::get_scale_kernel(base::DeviceType::kDeviceCUDA)(0.5f, t1, nullptr);
-//   cudaDeviceSynchronize();
-//
-//   t1.to_cpu();
-//   for (int i = 0; i < size; ++i) {
-//     ASSERT_EQ(t1.index<float>(i), 1.f);
-//   }
-// }
-//
-// TEST(test_scale_cu, scale1_stream) {
-//   auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
-//   int32_t size = 32 * 151;
-//
-//   tensor::Tensor t1(base::DataType::kDataTypeFp32, size, true, alloc_cu);
-//   set_value_cu(static_cast<float*>(t1.get_buffer()->ptr()), size, 2.f);
-//   cudaStream_t stream;
-//   cudaStreamCreate(&stream);
-//   kernel::get_scale_kernel(base::DeviceType::kDeviceCUDA)(0.4f, t1, nullptr);
-//   cudaDeviceSynchronize();
-//
-//   t1.to_cpu();
-//   cudaStreamDestroy(stream);
-//
-//   for (int i = 0; i < size; ++i) {
-//     ASSERT_EQ(t1.index<float>(i), 0.8f);
-//   }
-// }
+TEST(test_scale_cu, scale1_nostream) {
+  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
+  int32_t size = 32 * 151;
+
+  tensor::Tensor t1 =
+      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {size}, alloc_cu);
+  std::vector<float> vec_2(size, 2.f);
+  cudaMemcpy(t1.get_buffer()->ptr(), vec_2.data(), size * sizeof(float),
+             cudaMemcpyHostToDevice);
+
+  kernel::get_scale_kernel(base::DeviceType::kDeviceCUDA)(0.5f, t1, nullptr);
+  cudaDeviceSynchronize();
+
+  t1.to_cpu();
+  for (int i = 0; i < size; ++i) {
+    ASSERT_EQ(t1.at<float>(i), 1.f);
+  }
+}
+
+TEST(test_scale_cu, scale1_stream) {
+  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
+  int32_t size = 32 * 151;
+
+  tensor::Tensor t1 =
+      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {size}, alloc_cu);
+  std::vector<float> vec_2(size, 2.f);
+  cudaMemcpy(t1.get_buffer()->ptr(), vec_2.data(), size * sizeof(float),
+             cudaMemcpyHostToDevice);
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+  kernel::get_scale_kernel(base::DeviceType::kDeviceCUDA)(0.4f, t1, nullptr);
+  cudaDeviceSynchronize();
+
+  t1.to_cpu();
+  cudaStreamDestroy(stream);
+
+  for (int i = 0; i < size; ++i) {
+    ASSERT_EQ(t1.at<float>(i), 0.8f);
+  }
+}
