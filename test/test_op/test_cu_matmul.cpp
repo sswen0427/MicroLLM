3362f26be7bd30f22f20547f442ad601bd6202c0
sswen0427 <799438891@qq.com>
2026-04-20 21:46:27 +0800

    fix ut

--- test/test_op/test_cu_matmul.cpp
+++ test/test_op/test_cu_matmul.cpp
@@ -5,9 +5,75 @@
 
 #include "base/buffer.h"
 #include "op/kernels/kernels_interface.h"
-using namespace kernel;
 
-TEST(CudaMatmulTest, matmul_linear_stream5) {
+TEST(CudaMatmulTest, NoStream1) {
+  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
+  auto alloc_cpu = base::CPUDeviceAllocatorFactory::get_instance();
+
+  tensor::Tensor input =
+      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {3}, alloc_cpu);
+  tensor::Tensor weight = tensor::Tensor::allocate(
+      base::DataType::kDataTypeFp32, {3, 3}, alloc_cpu);
+
+  input.at<float>(0) = float(1);
+  input.at<float>(1) = float(1);
+  input.at<float>(2) = float(-1);
+
+  for (int i = 1; i <= 9; ++i) {
+    weight.at<float>(i - 1) = float(i);
+  }
+  tensor::Tensor input_cpu = input.clone();
+  tensor::Tensor weight_cpu = weight.clone();
+
+  input.to_cuda(nullptr);
+  weight.to_cuda(nullptr);
+
+  tensor::Tensor out_cpu =
+      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {3}, alloc_cpu);
+
+  kernel::get_matmul_kernel(base::DeviceType::kDeviceCPU)(
+      input_cpu, weight_cpu, out_cpu, 1.f, nullptr);
+
+  ASSERT_EQ(out_cpu.at<float>(0), 0);
+  ASSERT_EQ(out_cpu.at<float>(1), 3);
+  ASSERT_EQ(out_cpu.at<float>(2), 6);
+}
+
+TEST(CudaMatmulTest, NoStream2) {
+  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
+  auto alloc_cpu = base::CPUDeviceAllocatorFactory::get_instance();
+
+  tensor::Tensor input =
+      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {3}, alloc_cpu);
+  tensor::Tensor weight = tensor::Tensor::allocate(
+      base::DataType::kDataTypeFp32, {3, 3}, alloc_cpu);
+
+  input.at<float>(0) = float(1);
+  input.at<float>(1) = float(1);
+  input.at<float>(2) = float(-1);
+
+  for (int i = 1; i <= 9; ++i) {
+    weight.at<float>(i - 1) = float(i);
+  }
+
+  input.to_cuda();
+  weight.to_cuda();
+
+  tensor::Tensor out_cu =
+      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {3}, alloc_cu);
+
+  kernel::get_matmul_kernel(base::DeviceType::kDeviceCUDA)(
+      input, weight, out_cu, 1.f, nullptr);
+
+  tensor::Tensor out_cpu = out_cu.clone();
+  out_cpu.to_cpu();
+
+  ASSERT_EQ(out_cpu.at<float>(0), 0);
+  ASSERT_EQ(out_cpu.at<float>(1), 3);
+  ASSERT_EQ(out_cpu.at<float>(2), 6);
+}
+
+TEST(CudaMatmulTest, Stream) {
   auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
   auto alloc_cpu = base::CPUDeviceAllocatorFactory::get_instance();
 
@@ -48,71 +114,4 @@ TEST(CudaMatmulTest, matmul_linear_stream5) {
   for (int i = 0; i < out_cu.size(); ++i) {
     ASSERT_EQ(out_cu.at<float>(i), out_cpu.at<float>(i));
   }
-}
-
-TEST(CudaMatmulTest, matmul_linear_course) {
-  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
-  auto alloc_cpu = base::CPUDeviceAllocatorFactory::get_instance();
-
-  tensor::Tensor input =
-      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {3}, alloc_cpu);
-  tensor::Tensor weight = tensor::Tensor::allocate(
-      base::DataType::kDataTypeFp32, {3, 3}, alloc_cpu);
-
-  input.at<float>(0) = float(1);
-  input.at<float>(1) = float(1);
-  input.at<float>(2) = float(-1);
-
-  for (int i = 1; i <= 9; ++i) {
-    weight.at<float>(i - 1) = float(i);
-  }
-  tensor::Tensor input_cpu = input.clone();
-  tensor::Tensor weight_cpu = weight.clone();
-
-  input.to_cuda(nullptr);
-  weight.to_cuda(nullptr);
-
-  tensor::Tensor out_cpu =
-      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {3}, alloc_cpu);
-
-  kernel::get_matmul_kernel(base::DeviceType::kDeviceCPU)(
-      input_cpu, weight_cpu, out_cpu, 1.f, nullptr);
-
-  ASSERT_EQ(out_cpu.at<float>(0), 0);
-  ASSERT_EQ(out_cpu.at<float>(1), 3);
-  ASSERT_EQ(out_cpu.at<float>(2), 6);
-}
-
-TEST(CudaMatmulTest, matmul_linear_course_cuda) {
-  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
-  auto alloc_cpu = base::CPUDeviceAllocatorFactory::get_instance();
-
-  tensor::Tensor input =
-      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {3}, alloc_cpu);
-  tensor::Tensor weight = tensor::Tensor::allocate(
-      base::DataType::kDataTypeFp32, {3, 3}, alloc_cpu);
-
-  input.at<float>(0) = float(1);
-  input.at<float>(1) = float(1);
-  input.at<float>(2) = float(-1);
-
-  for (int i = 1; i <= 9; ++i) {
-    weight.at<float>(i - 1) = float(i);
-  }
-
-  input.to_cuda();
-  weight.to_cuda();
-
-  tensor::Tensor out_cu =
-      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {3}, alloc_cu);
-
-  kernel::get_matmul_kernel(base::DeviceType::kDeviceCUDA)(
-      input, weight, out_cu, 1.f, nullptr);
-
-  tensor::Tensor out_cpu = out_cu.clone();
-  out_cpu.to_cpu();
-
-  ASSERT_EQ(out_cpu.at<float>(0), 0);
-  ASSERT_EQ(out_cpu.at<float>(1), 3);
-  ASSERT_EQ(out_cpu.at<float>(2), 6);
 }
