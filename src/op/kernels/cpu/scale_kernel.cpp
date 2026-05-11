fcd9950fdc1633c968ec9891eaae65774e316354
sswen0427 <799438891@qq.com>
2026-05-12 01:29:37 +0800

    fix bug

--- src/op/kernels/cpu/scale_kernel.cpp
+++ src/op/kernels/cpu/scale_kernel.cpp
@@ -8,6 +8,6 @@ void scale_inplace_cpu(float scale, const tensor::Tensor& tensor,
   CHECK(tensor.is_empty() == false);
   arma::fvec tensor_mat(const_cast<float*>(tensor.ptr<float>()), tensor.size(),
                         false, true);
-  tensor_mat = tensor_mat * scale;
+  tensor_mat *= scale;
 }
 }  // namespace kernel
