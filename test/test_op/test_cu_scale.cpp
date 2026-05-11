ad43c6c9dac4b84fa4de5950386541d801895749
sswen0427 <799438891@qq.com>
2026-05-12 01:38:06 +0800

    fix bug

--- test/test_op/test_cu_scale.cpp
+++ test/test_op/test_cu_scale.cpp
@@ -5,11 +5,11 @@
 #include "base/buffer.h"
 #include "op/kernels/kernels_interface.h"
 TEST(ScaleTest, Nostream) {
-  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
+  auto alloc_cpu = base::CPUDeviceAllocatorFactory::get_instance();
   int32_t size = 32 * 151;
 
-  tensor::Tensor t1 =
-      tensor::Tensor::allocate(base::DataType::kDataTypeFp32, {size}, alloc_cu);
+  tensor::Tensor t1 = tensor::Tensor::allocate(base::DataType::kDataTypeFp32,
+                                               {size}, alloc_cpu);
   std::vector<float> vec(size, 2.f);
   cudaMemcpy(t1.get_buffer()->ptr(), vec.data(), size * sizeof(float),
              cudaMemcpyHostToHost);
