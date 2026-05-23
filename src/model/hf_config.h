d6dd17b9304bcd20afb07e4e63cecbcb28e1302f
sswen0427 <799438891@qq.com>
2026-05-23 17:23:39 +0800

    update HfllamaConfig

--- src/model/hf_config.h
+++ src/model/hf_config.h
@@ -1,19 +1,17 @@
 #pragma once
 
 #include <absl/status/statusor.h>
-#include <nlohmann/json.hpp>
 
 #include <cstdint>
+#include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
 
 namespace model {
 
 struct HfLlamaConfig {
-  std::string name_or_path;
   std::vector<std::string> architectures;
   bool attention_bias = false;
-  double attention_dropout = 0.0;
   int32_t bos_token_id = -1;
   int32_t eos_token_id = -1;
   std::string hidden_act;
@@ -22,11 +20,9 @@ struct HfLlamaConfig {
   int32_t intermediate_size = 0;
   int32_t max_position_embeddings = 0;
   std::string model_type;
-  bool mlp_bias = false;
-  int32_t num_hidden_layers = 0;
   int32_t num_attention_heads = 0;
+  int32_t num_hidden_layers = 0;
   int32_t num_key_value_heads = 0;
-  int32_t pad_token_id = -1;
   int32_t pretraining_tp = 1;
   double rms_norm_eps = 0.0;
   nlohmann::json rope_scaling;
@@ -36,9 +32,6 @@ struct HfLlamaConfig {
   std::string transformers_version;
   bool use_cache = true;
   int32_t vocab_size = 0;
-  nlohmann::json raw_json;
-
-  std::string architecture() const;
 };
 
 absl::StatusOr<HfLlamaConfig> LoadHfLlamaConfig(const std::string& model_dir);
