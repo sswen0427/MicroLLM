9967b959cf1bf188f0c45dc5f440a3a7c56cdc65
sswen0427 <799438891@qq.com>
2026-05-23 17:08:12 +0800

    adjust

--- src/model/hf_config.cpp
+++ src/model/hf_config.cpp
@@ -11,24 +11,24 @@
 namespace model {
 
 void from_json(const nlohmann::json& json, HfLlamaConfig& config) {
-  config.model_type = json.at("model_type").get<std::string>();
-  config.hidden_size = json.at("hidden_size").get<int32_t>();
-  config.intermediate_size = json.at("intermediate_size").get<int32_t>();
-  config.num_hidden_layers = json.at("num_hidden_layers").get<int32_t>();
-  config.num_attention_heads = json.at("num_attention_heads").get<int32_t>();
-  config.vocab_size = json.at("vocab_size").get<int32_t>();
-
   if (json.contains("architectures") && json.at("architectures").is_array() &&
       !json.at("architectures").empty()) {
     config.architecture = json.at("architectures").front().get<std::string>();
   }
-  config.torch_dtype = json.value("torch_dtype", "");
+
+  config.hidden_size = json.at("hidden_size").get<int32_t>();
+  config.intermediate_size = json.at("intermediate_size").get<int32_t>();
+  config.max_position_embeddings = json.value("max_position_embeddings", 0);
+  config.model_type = json.at("model_type").get<std::string>();
+  config.num_attention_heads = json.at("num_attention_heads").get<int32_t>();
+  config.num_hidden_layers = json.at("num_hidden_layers").get<int32_t>();
   config.num_key_value_heads =
       json.value("num_key_value_heads", config.num_attention_heads);
-  config.max_position_embeddings = json.value("max_position_embeddings", 0);
   config.rms_norm_eps = json.value("rms_norm_eps", 0.0);
   config.rope_theta = json.value("rope_theta", 0.0);
   config.tie_word_embeddings = json.value("tie_word_embeddings", false);
+  config.torch_dtype = json.value("torch_dtype", "");
+  config.vocab_size = json.at("vocab_size").get<int32_t>();
 }
 
 absl::StatusOr<HfLlamaConfig> LoadHfLlamaConfig(const std::string& model_dir) {
