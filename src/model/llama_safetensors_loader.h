5266b9f72aa6cf283c50263db951060ed2c3929d
sswen0427 <799438891@qq.com>
2026-05-24 13:54:42 +0800

    load model

---
+++ src/model/llama_safetensors_loader.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <absl/status/statusor.h>
+#include <safetensors.hh>
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+#include "model/llama_tensor_names.h"
+#include "tensor/tensor.h"
+
+namespace model {
+
+class LlamaSafetensorsLoader {
+ public:
+  static absl::StatusOr<std::unique_ptr<LlamaSafetensorsLoader>> Open(
+      const std::string& safetensors_path);
+
+  absl::StatusOr<tensor::Tensor> LoadTensor(
+      const std::string& tensor_name) const;
+
+  absl::StatusOr<tensor::Tensor> LoadGlobalTensor(
+      LlamaTensorKind kind) const;
+
+  absl::StatusOr<tensor::Tensor> LoadLayerTensor(
+      int32_t layer, LlamaTensorKind kind) const;
+
+  [[nodiscard]] size_t TensorCount() const;
+
+ private:
+  explicit LlamaSafetensorsLoader(
+      std::unique_ptr<safetensors::safetensors_t> safetensors);
+
+  std::unique_ptr<safetensors::safetensors_t> safetensors_;
+};
+
+}  // namespace model
