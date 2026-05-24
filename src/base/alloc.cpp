9246fd4c1a4542308e9ffb9c0ebb0a489a046795
sswen0427 <799438891@qq.com>
2026-05-24 12:05:02 +0800

    remove alloc_cpu

--- src/base/alloc.cpp
+++ src/base/alloc.cpp
@@ -4,10 +4,51 @@
 #include <glog/logging.h>
 
 #include <cstring>
+#include <cstdlib>
 
 #include "base/cuda_check.h"
 
 namespace base {
+
+CPUDeviceAllocator::CPUDeviceAllocator()
+    : DeviceAllocator(DeviceType::kDeviceCPU) {}
+
+void* CPUDeviceAllocator::allocate(std::size_t byte_size) const {
+  CHECK(byte_size > 0) << "CPUDeviceAllocator::allocate(): byte_size is 0";
+  const size_t alignment = (byte_size >= 1024) ? 32 : 16;
+  const size_t aligned_size = (byte_size + alignment - 1) & ~(alignment - 1);
+  void* data = std::aligned_alloc(alignment, aligned_size);
+  if (data == nullptr) {
+    LOG(ERROR) << "std::aligned_alloc failed! "
+               << "(alignment: " << alignment
+               << ", original size: " << byte_size
+               << ", padded size: " << aligned_size << ")";
+    return nullptr;
+  }
+  return data;
+}
+
+void CPUDeviceAllocator::release(void* ptr) const {
+  if (ptr) {
+    std::free(ptr);
+  }
+}
+
+CUDADeviceAllocator::CUDADeviceAllocator()
+    : DeviceAllocator(DeviceType::kDeviceCUDA) {}
+
+void* CUDADeviceAllocator::allocate(std::size_t byte_size) const {
+  CHECK(byte_size > 0) << "CUDADeviceAllocator::allocate(): byte_size is 0";
+
+  void* ptr = nullptr;
+  CHECK_CUDA(cudaMalloc(&ptr, byte_size));
+  return ptr;
+}
+
+void CUDADeviceAllocator::release(void* ptr) const {
+  CHECK(ptr) << "CUDADeviceAllocator::release(): ptr is nullptr";
+  CHECK_CUDA(cudaFree(ptr));
+}
 
 std::shared_ptr<DeviceAllocator> GetDeviceAllocator(DeviceType device_type) {
   static std::shared_ptr<DeviceAllocator> cpu_allocator =
