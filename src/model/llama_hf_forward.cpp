2286a4548606178dbf07dc94fc46f25a05a6bd18
sswen0427 <799438891@qq.com>
2026-05-29 01:59:17 +0800

    use second

--- src/model/llama_hf_forward.cpp
+++ src/model/llama_hf_forward.cpp
@@ -350,24 +350,24 @@ void LlamaForwardProfile::Log() const {
   LOG(INFO) << "  Forward Stats:";
   LOG(INFO) << "    forward_calls=" << forward_calls;
   LOG(INFO) << "    Embedding & Attention:";
-  LOG(INFO) << "      embedding_ms=" << embedding_ms;
-  LOG(INFO) << "      attention_norm_ms=" << attention_norm_ms;
-  LOG(INFO) << "      qkv_proj_ms=" << qkv_proj_ms;
-  LOG(INFO) << "      rope_ms=" << rope_ms;
-  LOG(INFO) << "      kv_cache_ms=" << kv_cache_ms;
-  LOG(INFO) << "      attention_ms=" << attention_ms;
-  LOG(INFO) << "      attention_output_proj_ms=" << attention_output_proj_ms;
-  LOG(INFO) << "      attention_residual_ms=" << attention_residual_ms;
+  LOG(INFO) << "      embedding_s=" << embedding_ms / 1000.0;
+  LOG(INFO) << "      attention_norm_s=" << attention_norm_ms / 1000.0;
+  LOG(INFO) << "      qkv_proj_s=" << qkv_proj_ms / 1000.0;
+  LOG(INFO) << "      rope_s=" << rope_ms / 1000.0;
+  LOG(INFO) << "      kv_cache_s=" << kv_cache_ms / 1000.0;
+  LOG(INFO) << "      attention_s=" << attention_ms / 1000.0;
+  LOG(INFO) << "      attention_output_proj_s=" << attention_output_proj_ms / 1000.0;
+  LOG(INFO) << "      attention_residual_s=" << attention_residual_ms / 1000.0;
   LOG(INFO) << "    FFN:";
-  LOG(INFO) << "      ffn_norm_ms=" << ffn_norm_ms;
-  LOG(INFO) << "      ffn_up_gate_proj_ms=" << ffn_up_gate_proj_ms;
-  LOG(INFO) << "      swiglu_ms=" << swiglu_ms;
-  LOG(INFO) << "      ffn_down_proj_ms=" << ffn_down_proj_ms;
-  LOG(INFO) << "      ffn_residual_ms=" << ffn_residual_ms;
+  LOG(INFO) << "      ffn_norm_s=" << ffn_norm_ms / 1000.0;
+  LOG(INFO) << "      ffn_up_gate_proj_s=" << ffn_up_gate_proj_ms / 1000.0;
+  LOG(INFO) << "      swiglu_s=" << swiglu_ms / 1000.0;
+  LOG(INFO) << "      ffn_down_proj_s=" << ffn_down_proj_ms / 1000.0;
+  LOG(INFO) << "      ffn_residual_s=" << ffn_residual_ms / 1000.0;
   LOG(INFO) << "    Final:";
-  LOG(INFO) << "      final_norm_ms=" << final_norm_ms;
-  LOG(INFO) << "      lm_head_ms=" << lm_head_ms;
-  LOG(INFO) << "      argmax_ms=" << argmax_ms;
+  LOG(INFO) << "      final_norm_s=" << final_norm_ms / 1000.0;
+  LOG(INFO) << "      lm_head_s=" << lm_head_ms / 1000.0;
+  LOG(INFO) << "      argmax_s=" << argmax_ms / 1000.0;
 }
 
 }  // namespace model
