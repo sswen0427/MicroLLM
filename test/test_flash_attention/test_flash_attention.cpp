d152fc1b6f315d9fe569de4cac73973e4e2ee982
sswen0427 <799438891@qq.com>
2026-06-19 00:44:49 +0800

    attention_v2_no_p_matrix

--- test/test_flash_attention/test_flash_attention.cpp
+++ test/test_flash_attention/test_flash_attention.cpp
@@ -7,13 +7,12 @@ using std::vector;
 
 // Q, K, V: [N, D]
 // O:       [N, D]
-void attention_v1_full_matrices(const vector<float>& Q, const vector<float>& K,
-                                const vector<float>& V, vector<float>& O, int N,
-                                int D) {
+void attention_v2_no_p_matrix(const vector<float>& Q, const vector<float>& K,
+                              const vector<float>& V, vector<float>& O, int N,
+                              int D) {
   float scale = 1.0f / std::sqrt(static_cast<float>(D));
 
   vector<float> S(N * N, 0.0f);
-  vector<float> P(N * N, 0.0f);
   O.assign(N * D, 0.0f);
 
   // 1. S = QK^T / sqrt(D)
@@ -27,7 +26,7 @@ void attention_v1_full_matrices(const vector<float>& Q, const vector<float>& K,
     }
   }
 
-  // 2. P = softmax(S), row by row
+  // 2. For each row, softmax and immediately multiply by V.
   for (int i = 0; i < N; ++i) {
     float row_max = -std::numeric_limits<float>::infinity();
 
@@ -38,22 +37,14 @@ void attention_v1_full_matrices(const vector<float>& Q, const vector<float>& K,
     float row_sum_exp = 0.0f;
 
     for (int j = 0; j < N; ++j) {
-      float value = std::exp(S[i * N + j] - row_max);
-      P[i * N + j] = value;
-      row_sum_exp += value;
+      row_sum_exp += std::exp(S[i * N + j] - row_max);
     }
 
-    for (int j = 0; j < N; ++j) {
-      P[i * N + j] /= row_sum_exp;
-    }
-  }
-
-  // 3. O = P V
-  for (int i = 0; i < N; ++i) {
     for (int d = 0; d < D; ++d) {
       float out = 0.0f;
       for (int j = 0; j < N; ++j) {
-        out += P[i * N + j] * V[j * D + d];
+        float prob = std::exp(S[i * N + j] - row_max) / row_sum_exp;
+        out += prob * V[j * D + d];
       }
       O[i * D + d] = out;
     }
