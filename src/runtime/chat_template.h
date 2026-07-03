a3f0210b612adfe1ab00ec11b31c3b1bfe6ff138
sswen <60056687+sswen0427@users.noreply.github.com>
2026-07-04 00:55:42 +0800

    Chat model (#72)
    * chat mode
    
    * format
    
    * chat mode
    
    * max_new_tokens = 256
    
    * trim space

---
+++ src/runtime/chat_template.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace runtime {
+
+enum class ChatRole {
+  kSystem,
+  kUser,
+  kAssistant,
+};
+
+struct ChatMessage {
+  ChatRole role = ChatRole::kUser;
+  std::string content;
+};
+
+std::string FormatTinyLlamaChatPrompt(const std::vector<ChatMessage>& messages);
+
+}  // namespace runtime
